dfb533e4a16cc96373712575104f8d962dc69ea1
Kamie Short <kamie.short@gmail.com>
2022-03-07 15:47:51 -0800

    one animal displaying

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import './Animal.css';
+// import '.Animal.js';
 
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div className="animal">
+      <span className="name">${props.name}</span>
+      <span>${props.says}</span>
+      <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Animal from '../Animal/Animal.js';
+import animals from '../../data';
 import './Main.css';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main>
+      <Animal type="cow" says="moo" name="Bessy" top="225px" left="20%" />
+    </main>
+  );
 }
